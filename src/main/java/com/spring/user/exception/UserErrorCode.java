f7fe7365d505c0222a8f6a0db8edad0ffdb10f40
dudxo <father8475@naver.com>
2025-02-18 20:47:45 +0900

    feat: 회원 Role 필터링 오류코드 추가

--- src/main/java/com/spring/user/exception/UserErrorCode.java
+++ src/main/java/com/spring/user/exception/UserErrorCode.java
@@ -11,7 +11,8 @@ public enum UserErrorCode implements ErrorCode {
 
 	NOT_FOUND_USER("해당 회원을 찾을 수 없습니다.", "USER_001"),
 	DUPLICATED_EMAIL("중복된 이메일입니다.", "USER_002"),
-	INVALID_ROLE("유효하지 않은 역할입니다.", "USER_003");
+	INVALID_ROLE("유효하지 않은 역할입니다.", "USER_003"),
+	NOT_FOUND_FILTERED_USER("필터에 일치하는 회원을 찾을 수 없습니다.", "USER_004");
 
 	private final String message;
 	private final String code;
