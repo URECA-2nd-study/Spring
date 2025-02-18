bf4fdb0f58b87a7e3bfd575064770af411071e48
dudxo <father8475@naver.com>
2025-02-18 20:48:01 +0900

    feat: User QueryDSL 추가

--- src/main/java/com/spring/user/repository/UserRepository.java
+++ src/main/java/com/spring/user/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.spring.user.domain.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserCustom{
 	boolean existsByEmail(String email);
 
 	Optional<User> findByEmail(String email);
