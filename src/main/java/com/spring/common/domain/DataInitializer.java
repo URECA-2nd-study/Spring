5a13b8d903827eac1e238d63b844e7ac15179181
heejjinkim <hjhj12@dgu.ac.kr>
2025-02-19 15:24:01 +0900

    feat: 유저 더미 데이터 수정
    role에 따라 admin, member 각각 10개씩 생성

--- src/main/java/com/spring/common/domain/DataInitializer.java
+++ src/main/java/com/spring/common/domain/DataInitializer.java
@@ -3,8 +3,8 @@ package com.spring.common.domain;
 import com.spring.user.domain.Role;
 import com.spring.user.domain.User;
 import com.spring.user.repository.UserRepository;
-import java.util.Arrays;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -12,22 +12,46 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataInitializer implements ApplicationRunner {
 
+    private static final int ADMIN_CNT = 10;
+    private static final int MEMBER_CNT = 10;
+
     private final UserRepository userRepository;
 
-    @Autowired
-    public DataInitializer(UserRepository userRepository){
+    public DataInitializer(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        if(userRepository.count() == 0){
-            userRepository.saveAll(Arrays.asList(
-                    User.of("1@email.com","password","user1", Role.ADMIN),
-                    User.of("2@email.com","password","user2", Role.ADMIN),
-                    User.of("3@email.com","password","user4", Role.MEMBER),
-                    User.of("4@email.com","password","user1", Role.MEMBER)
-            ));
+        if (userRepository.count() == 0) {
+            userRepository.saveAll(createAdminUsers());
+            userRepository.saveAll(createMemberUsers());
         }
+    }
+
+    private List<User> createAdminUsers() {
+        List<User> adminUsers = new ArrayList<>();
+
+        for (int i = 1; i <= ADMIN_CNT; i++) {
+            adminUsers.add(createUser(i, Role.ADMIN));
+        }
+        return adminUsers;
+    }
+
+    private List<User> createMemberUsers() {
+        List<User> memberUsers = new ArrayList<>();
+
+        for (int i = ADMIN_CNT + 1; i <= ADMIN_CNT + MEMBER_CNT; i++) {
+            memberUsers.add(createUser(i, Role.MEMBER));
+        }
+        return memberUsers;
+    }
+
+    private User createUser(int index, Role role) {
+        String email = index + "@gmail.com";
+        String password = "password" + index;
+        String name = "user" + index;
+
+        return User.of(email, password, name, role);
     }
 }
