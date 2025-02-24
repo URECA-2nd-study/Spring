fbc691fa8c266d9405979e08f97a7c333a3c8060
heejjinkim <hjhj12@dgu.ac.kr>
2025-02-24 17:16:43 +0900

    refactor: UserFixture 생성 함수 수정

--- src/test/java/com/spring/fixture/UserFixture.java
+++ src/test/java/com/spring/fixture/UserFixture.java
@@ -4,30 +4,12 @@ import com.spring.user.domain.Role;
 import com.spring.user.domain.User;
 
 public class UserFixture {
-    public static User memberUser1() {
+    public static User createUser(String email, Role role){
         return User.of(
-                "member1@gamil.com",
+                email,
                 "pwd",
-                "memberUser1",
-                Role.MEMBER
-        );
-    }
-
-    public static User memberUser2() {
-        return User.of(
-                "member2@gamil.com",
-                "pwd",
-                "memberUser2",
-                Role.MEMBER
-        );
-    }
-
-    public static User adminUser1() {
-        return User.of(
-                "admin1@gmail.com",
-                "pwd",
-                "adminUser1",
-                Role.ADMIN
+                "테스트유저",
+                role
         );
     }
 }
--- src/test/java/com/spring/user/repository/UserRepositoryTest.java
+++ src/test/java/com/spring/user/repository/UserRepositoryTest.java
@@ -1,12 +1,11 @@
 package com.spring.user.repository;
 
-import static com.spring.fixture.UserFixture.adminUser1;
-import static com.spring.fixture.UserFixture.memberUser1;
-import static com.spring.fixture.UserFixture.memberUser2;
+import static com.spring.fixture.UserFixture.createUser;
+import static com.spring.user.domain.Role.ADMIN;
+import static com.spring.user.domain.Role.MEMBER;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.spring.common.domain.QuerydslConfig;
-import com.spring.user.domain.Role;
 import com.spring.user.domain.User;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -28,7 +27,7 @@ class UserRepositoryTest {
     @DisplayName("유저가 정상적으로 가입")
     void joinUser() {
         // given
-        User memberUser = memberUser1();
+        User memberUser = createUser("1@gmail.com", MEMBER);
 
         // when
         User savedUser = userRepository.save(memberUser);
@@ -41,33 +40,34 @@ class UserRepositoryTest {
     @DisplayName("멤버 역할 유저 정상적으로 검색")
     void getUserByMemberRole() {
         // given
-        User memberUser1 = memberUser1();
-        User memberUser2 = memberUser2();
-        User adminUser = adminUser1();
+        User memberUser1 = createUser("1@gmail.com", MEMBER);
+        User memberUser2 = createUser("2@gmail.com", MEMBER);
+        User adminUser = createUser("3@gmail.com", ADMIN);
         userRepository.saveAll(List.of(memberUser1, memberUser2, adminUser));
 
         // when
-        List<User> findMemberUsers = userRepository.findAllByRole(Role.MEMBER);
+        List<User> findMemberUsers = userRepository.findAllByRole(MEMBER);
 
         // then
         assertThat(findMemberUsers).hasSize(2);
-        assertThat(findMemberUsers).allMatch(user -> user.getRole() == Role.MEMBER);
+        assertThat(findMemberUsers).allMatch(user -> user.getRole() == MEMBER);
     }
 
     @Test
     @DisplayName("어드민 역할 유저 정상적으로 검색")
     void getUserByAdminRole() {
         // given
-        User memberUser1 = memberUser1();
-        User memberUser2 = memberUser2();
-        User adminUser = adminUser1();
-        userRepository.saveAll(List.of(memberUser1, memberUser2, adminUser));
+        User memberUser1 = createUser("1@gmail.com", MEMBER);
+        User memberUser2 = createUser("2@gmail.com", MEMBER);
+        User adminUser1 = createUser("3@gmail.com", ADMIN);
+        User adminUser2 = createUser("4@gmail.com", ADMIN);
+        userRepository.saveAll(List.of(memberUser1, memberUser2, adminUser1, adminUser2));
 
         // when
-        List<User> findAdminUsers = userRepository.findAllByRole(Role.ADMIN);
+        List<User> findAdminUsers = userRepository.findAllByRole(ADMIN);
 
         // then
         assertThat(findAdminUsers).hasSize(2);
-        assertThat(findAdminUsers).allMatch(user -> user.getRole() == Role.ADMIN);
+        assertThat(findAdminUsers).allMatch(user -> user.getRole() == ADMIN);
     }
 }
