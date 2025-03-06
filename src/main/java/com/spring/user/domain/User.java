7391c4e507d2719a356521ea675a2e403b796b4f
yereumi <lovelyyeji01@gmail.com>
2025-03-06 09:51:18 +0900

    feat: 트랜잭션 격리 수준 서비스 코드 추가

--- src/main/java/com/spring/user/domain/User.java
+++ src/main/java/com/spring/user/domain/User.java
@@ -1,18 +1,14 @@
 package com.spring.user.domain;
 
 import com.spring.common.domain.TimeBaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "Users")
@@ -22,7 +18,7 @@ public class User extends TimeBaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "user_id")
+	@Column(name = "id")
 	private Long id;
 
 	@Column(name = "email", nullable = false, unique = true)
@@ -38,26 +34,44 @@ public class User extends TimeBaseEntity {
 	@Column(name = "role", nullable = false)
 	private Role role;
 
+	@Column(name ="point", nullable = false)
+	private BigDecimal point;
+
 	@Builder(access = AccessLevel.PRIVATE)
-	private User(String email, String password, String name, Role role) {
+	private User(String email, String password, String name, Role role, BigDecimal point) {
 		this.email = email;
 		this.password = password;
 		this.name = name;
 		this.role = role;
+		this.point = point;
 	}
 
 	public static User of(String email, String password, String name, Role role) {
 		return User.builder()
-			.email(email)
-			.password(password)
-			.name(name)
-			.role(role)
-			.build();
+				.email(email)
+				.password(password)
+				.name(name)
+				.role(role)
+				.build();
+	}
+
+	public static User of(String email, String password, String name, Role role, BigDecimal point) {
+		return User.builder()
+				.email(email)
+				.password(password)
+				.name(name)
+				.role(role)
+				.point(point)
+				.build();
 	}
 
 	public void updateInfo(String email, String name, String role) {
 		this.email = email;
 		this.name = name;
 		this.role = Role.of(role);
+	}
+
+	public void updatePoint(BigDecimal point) {
+		this.point = this.point.add(point);
 	}
 }
